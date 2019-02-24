d6a71c0d5266b368cff85957069198fedcbef4c7
Jonrosario5 <Jonathan.Rosario550@gmail.com>
2019-02-24 11:53:05 -0800

    Added Models folder

---
+++ models/album.js
@@ -0,0 +1,17 @@
+const mongoose = require(`mongoose`);
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+    name: String,
+    artistPic: String,
+})
+
+const AlbumSchema = new Schema({
+    name: String,
+    releaseDate: String,
+    albumPic:String,
+    artist: ArtistSchema
+});
+
+const Album = mongoose.model(`Album`, AlbumSchema);
+module.exports = Album;
