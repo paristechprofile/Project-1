a084e2a44119001f0246b26ddf21dda30e60fe10
Paris Taylor <paristechprofile@gmail.com>
2019-02-24 14:24:32 -0800

    adding a few changes to the app.js file. Seemed to have redunant code. Refactored and shared changes with Jon

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,18 +1,12 @@
 $(document).ready(function () {
-
     let albumList = []
-
     const newAlbum = dbUser => {
         
         console.log("dbUser", dbUser);
-
         albumList = dbUser.albums;
-
-
         let artistName = albumList[i].artist.name;
         let albumName = albumList[i].name;
         let albumPic = albumList[i].albumPic;
-
         let userId = albumList[i]._id;
         console.log(userId);
         console.log(albumPic)
@@ -28,17 +22,13 @@ $(document).ready(function () {
                 </div>
             </div>
         `;
-
         const deleteAlbum = (data) => {
             console.log("this is new data ", data);
             $("#albumStack").empty();
         }
-
         // appending th html in the div
         $('#albumCardTarget').append(htmlFragment);
-
         console.log($("#deleteButton"))
-
         // Add an Event Listener and 
         // $(".btn-danger").click(function (e) {
         //     console.log("which event is firing 1")
@@ -51,20 +41,15 @@ $(document).ready(function () {
         //     })
         // })
         // console.log(albumList);
-
     };
-
     //  Get all albums and render
     const findAlbumById = dbUser => {
         console.log("dbUser", dbUser);
-
         albumList = dbUser.albums;
-
         for (let i = 0; i < albumList.length; i++) {
             let artistName = albumList[i].artist.name;
             let albumName = albumList[i].name;
             let albumPic = albumList[i].albumPic;
-
             let userId = albumList[i]._id;
             console.log(userId);
             let htmlFragment = `
@@ -83,7 +68,6 @@ $(document).ready(function () {
             $('#albumCardTarget').append(htmlFragment);
             }
         }
-
     //   Make an Ajax call to get user by id
     $.ajax({
         method: "GET",
@@ -91,14 +75,11 @@ $(document).ready(function () {
         success: findAlbumById,
         error: err => console.log(err)
     })
-
     // Add an Event handler on the submit button
     $('#albumForm').on("submit", function (e) {
         console.log("Submit button is firing")
         
-
         let formData = $(this).serialize();
-
         $.ajax({
             method: 'POST',
             url: '/api/user/5c72fd3dfe78139a8f10ccb5/albums',
@@ -109,22 +90,19 @@ $(document).ready(function () {
                 $("input[type=text], textarea").val("");
             }
         });
-
-
     })
+    // Delete Albums from Page
+    $("#albumStack").on("click",(function(){
+        console.log("Targeted");
+    }))
     
-
     const createNewAlbum = json => {
         console.log(json)
-
         let userAlbums = json.albums;
-
         let newAlbumName = userAlbums[userAlbums.length - 1].name;
         let newAlbumArtist = userAlbums[userAlbums.length - 1].artist || "Unknown Artist";
         let newAlbumid = userAlbums[userAlbums.length - 1]._id
-
         render(newAlbumName, newAlbumArtist, newAlbumid);
-
     }
     //  Render the new album
     const render = (newAlbumName, newAlbumArtist, newAlbumid) => {
@@ -140,10 +118,8 @@ $(document).ready(function () {
                 </div>
             </div>
         `;
-
         // appending th html in the div
         $('#albumCardTarget').append(htmlFragment);
-
     };
     //  Get all album
     const getAllTheAlbum = (album) => {
@@ -152,7 +128,5 @@ $(document).ready(function () {
     // // Do an Ajax call  to create new album
     // $('#inputSubmit').on('submit', function (e) {
     //     e.preventDefault();
-
     // });
-
 })
